9df99cb5e9791e6c9ff49dbf55968b51eaacb347
KNU Cho <k5435n@knu.ac.kr>
2016-03-16 15:20:19 +0900

    First

---
+++ Java_Project/src/sample_0317/HelloWorld.java
@@ -0,0 +1,11 @@
+package sample_0317;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
