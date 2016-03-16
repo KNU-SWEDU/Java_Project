54516b436eb10d3759f11f950ee4bb4da16112e8
KNU Cho <k5435n@knu.ac.kr>
2016-03-16 21:47:17 +0900

    Add Calculator

---
+++ Java_Project/src/sample_0317/Calculator.java
@@ -0,0 +1,14 @@
+package sample_0317;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5, y=7, result;
+		
+		result = x + y;
+		
+		System.out.print(result);
+	}
+
+}
