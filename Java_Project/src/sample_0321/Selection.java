26ea989304e66d9495c1da8646584bb83b347132
KNU Cho <k5435n@knu.ac.kr>
2016-03-17 14:25:44 +0900

    Adding Selection.java

---
+++ Java_Project/src/sample_0321/Selection.java
@@ -0,0 +1,22 @@
+package sample_0321;
+
+import java.util.*;
+
+public class Selection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int con = input.nextInt();
+		
+		if(con % 2 == 0){
+			System.out.println("JJJJ");
+		}else {
+			System.out.println("oooooo");
+		}
+
+	}
+
+}
