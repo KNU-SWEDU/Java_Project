ed94aeed6ce6324134fdb1c0cf7eec5e13293efa
KNU Cho <k5435n@knu.ac.kr>
2016-03-18 14:52:58 +0900

    Update Selection.java

--- Java_Project/src/sample_0321/Selection.java
+++ Java_Project/src/sample_0321/Selection.java
@@ -7,15 +7,17 @@ public class Selection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner input = new Scanner(System.in);
+		Scanner Input = new Scanner(System.in);
 		
-		int con = input.nextInt();
+		int con = Input.nextInt();
 		
 		if(con % 2 == 0){
 			System.out.println("JJJJ");
 		}else {
 			System.out.println("oooooo");
 		}
+		
+		Input.close();
 
 	}
 
